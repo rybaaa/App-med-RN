d83004e2ee456ac7f8bbc944df1ed4204089ea02
rybaaa <bolshayaryba22@gmail.com>
2023-02-08 22:22:14 +0300

    start app

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Dimensions, ImageBackground, StyleSheet, Text, View} from 'react-native';
+
+const {width, height} = Dimensions.get('screen')
+
+const WIDTH = width
+const HEIGHT = height
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <ImageBackground resizeMode="cover" style={styles.image}>
+      </ImageBackground>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +23,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image:{
+    backgroundColor: '#1E63EE',
+    width:WIDTH,
+    height:HEIGHT
+  }
 });
